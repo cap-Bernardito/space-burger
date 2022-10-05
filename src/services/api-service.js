647882d0efbdd8622fd9a6e366d483d2fc48ca2f
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-10-05 17:02:58 +0300

    refactor: ApiService

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -2,24 +2,29 @@ import { data as fakeData } from '../utils/data';
 import { TYPES_OF_INGRIDIENTS } from '../utils/constants';
 
 class ApiService {
-  _apiRequest = 'https://norma.nomoreparties.space/api/ingredients';
+  _baseApiUrl = 'https://norma.nomoreparties.space/api';
   _isFakeData = process.env.REACT_APP_DATA_SOURCE === 'fake-data';
 
-  async getResource() {
+  async getResource(endpoint) {
     if (this._isFakeData) {
       return { data: fakeData };
     }
 
-    const res = await fetch(this._apiRequest);
+    if (!endpoint) {
+      throw new Error('Endpoint in "ApiService.getResource" function is not valid');
+    }
+
+    const request = `${this._baseApiUrl}${endpoint}`;
+    const res = await fetch(request);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${this._apiRequest}` + `, received ${res.status}`);
+      throw new Error(`Could not fetch ${request}, received ${res.status}`);
     }
     return await res.json();
   }
 
   async getBurgerIngridientsByType() {
-    const { data } = await this.getResource();
+    const { data } = await this.getResource('/ingredients/');
 
     return this._transformIngridientsList(data);
   }
