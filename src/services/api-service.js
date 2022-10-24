e002531dc1fce179035c6d1ec9ae918bca34f306
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-10-24 07:28:30 +0300

    feat: добавлен API для работы с пользователем и токеном

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,21 +1,90 @@
 import { data as fakeData } from "../utils/data";
 
 class ApiService {
+  _accessToken = null;
+  _refreshToken = null;
   _baseApiUrl = "https://norma.nomoreparties.space/api";
   _isFakeData = process.env.REACT_APP_DATA_SOURCE === "fake-data";
+  _endpoint = {
+    access_token: {
+      create: "/auth/login/",
+      delete: "/auth/logout/",
+      update: "/auth/token/",
+    },
+    user: {
+      create: "/auth/register/",
+      reset: "/password-reset/",
+      update: "/password-reset/reset/",
+      get: "/auth/user/",
+    },
+    ingredients: {
+      get: "/ingredients/",
+    },
+    orders: {
+      create: "/orders/",
+    },
+  };
+  _defaultFetchProperties = {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  };
 
-  getResource = async (endpoint, requestInit = {}) => {
+  request = async (endpoint, fetchProperties = {}, authProperties = {}) => {
     if (!endpoint) {
       throw new Error('Endpoint in "ApiService.getResource" function is not valid');
     }
 
+    const requestInit = { ...this._defaultFetchProperties, ...fetchProperties, ...authProperties };
     const request = `${this._baseApiUrl}${endpoint}`;
-    const res = await fetch(request, requestInit);
+    const response = await fetch(request, requestInit);
+    const result = await response.json();
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${request}, received ${res.status}`);
+    if (!result.success) {
+      throw new Error(result.message);
     }
-    return await res.json();
+
+    if (!response.ok) {
+      throw new Error(`Could not fetch ${request}, received ${response.status}`);
+    }
+
+    return result;
+  };
+
+  requestWithAuth = async (endpoint, fetchProperties = {}) => {
+    const { accessToken, refreshToken } = this._getTokens();
+    const authProperties = {};
+
+    if (accessToken) {
+      authProperties["Authorization"] = accessToken;
+    }
+
+    const result = await this.request(endpoint, fetchProperties, authProperties);
+
+    if (result.success) {
+      return result;
+    }
+
+    if (result.message === "jwt expired") {
+      try {
+        await this.updateAccessToken(refreshToken);
+
+        return this.requestWithAuth(endpoint, fetchProperties);
+      } catch {
+        this._setTokens({
+          accessToken: null,
+          refreshToken: null,
+        });
+      }
+    }
+
+    throw new Error(result.message);
   };
 
   getBurgerIngredients = async () => {
@@ -27,7 +96,7 @@ class ApiService {
       );
     }
 
-    const { data } = await this.getResource("/ingredients/");
+    const { data } = await this.request(this._endpoint.ingredients.get);
 
     return this._transformData(data);
   };
@@ -54,17 +123,91 @@ class ApiService {
         ingredients: ingredientIds,
       };
 
-      data = await this.getResource("/orders/", {
+      data = await this.request(this._endpoint.orders.create, {
         method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
         body: JSON.stringify(requestBody),
       });
     }
 
     return this._transformOrderInfo(data);
+  };
+
+  createUser = async (userInfo) => {
+    const result = await this.request(this._endpoint.user.create, {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    });
+
+    this._setTokens(result);
+
+    return result;
+  };
+
+  updateUser = async (userInfo) => {
+    const result = await this.request(this._endpoint.user.update, {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    });
+
+    this._setTokens(result);
+
+    return result;
+  };
+
+  resetUser = async (email) => {
+    return this.request(this._endpoint.user.reset, {
+      method: "POST",
+      body: JSON.stringify({ email }),
+    });
+  };
+
+  getUser = async () => {
+    return this.request(this._endpoint.user.get);
+  };
+
+  createAccessToken = async (tokenInfo) => {
+    const result = await this.request(this._endpoint.access_token.create, {
+      method: "POST",
+      body: JSON.stringify(tokenInfo),
+    });
+
+    this._setTokens(result);
+
+    return result;
+  };
+
+  updateAccessToken = async (refreshToken) => {
+    const result = await this.request(this._endpoint.access_token.update, {
+      method: "POST",
+      body: JSON.stringify({ token: refreshToken }),
+    });
+
+    this._setTokens(result);
+
+    return result;
+  };
+
+  deleteAccessToken = async (refreshToken) => {
+    return this.request(this._endpoint.access_token.delete, {
+      method: "POST",
+      body: JSON.stringify({ token: refreshToken }),
+    });
+  };
+
+  _setTokens = (tokens = {}) => {
+    this._accessToken = tokens.accessToken;
+    this._refreshToken = tokens.refreshToken;
+  };
+
+  _getTokens = () => {
+    if (this._accessToken && this._refreshToken) {
+      return {
+        accessToken: this._accessToken,
+        refreshToken: this._refreshToken,
+      };
+    }
+
+    throw new Error("Access token is not available");
   };
 
   _transformData(data) {
