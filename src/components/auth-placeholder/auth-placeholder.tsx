c8a9d2af8e98a83030e4f312387cda4414d6f6aa
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-11-15 19:46:29 +0300

    feat: добавлены типы для AuthPlaceholder

---
+++ src/components/auth-placeholder/auth-placeholder.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+const AuthPlaceholder: FC = () => <div className="container text-center pt-20">Аутентификация пользователя...</div>;
+
+export default AuthPlaceholder;
