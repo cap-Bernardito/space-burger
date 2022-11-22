ccb3338aa40922ac87d7c32709cb9571427d5209
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-11-22 11:07:01 +0300

    feat: добавлены типы для PageTitle

---
+++ src/components/page-title/page-title.tsx
@@ -0,0 +1,27 @@
+import classNames from "classnames";
+
+import { useScreenTest } from "hooks";
+
+import styles from "./page-title.module.scss";
+
+type Props = {
+  titleMobile?: string;
+  titleDesktop?: string;
+};
+
+const PageTitle: React.FC<Props> = ({ titleMobile, titleDesktop }) => {
+  const isSmallScreen = useScreenTest();
+  const commonClass = classNames(styles.root, "text_type_main-medium text-center");
+
+  if (!titleMobile && !titleDesktop) {
+    return null;
+  }
+
+  if (isSmallScreen) {
+    return titleMobile ? <h1 className={classNames(commonClass, "mb-8")}>{titleMobile}</h1> : null;
+  }
+
+  return titleDesktop ? <h1 className={classNames(commonClass, "mb-5")}>{titleDesktop}</h1> : null;
+};
+
+export default PageTitle;
