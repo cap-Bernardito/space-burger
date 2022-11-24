cc73b1c47da0116d92c74716a4e62b1c7767477c
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-11-24 05:58:47 +0300

    feat: добавлены типы для страницы ProfileOrders

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,14 +1,11 @@
-import { PAGES_PROTYPES } from "utils/constants";
 import { setDocumentTitle } from "utils/utils";
 
 import PageTitle from "components/page-title/page-title";
 
-const ProfileOrders = ({ pageTitle }) => {
+const ProfileOrders: React.FC<TPageProps> = ({ pageTitle }) => {
   setDocumentTitle(pageTitle);
 
   return <PageTitle titleMobile={pageTitle} titleDesktop="История заказов" />;
 };
-
-ProfileOrders.propTypes = PAGES_PROTYPES;
 
 export default ProfileOrders;
