23e4debf9c9fa1d67276871e5b378c256e18379c
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-12-02 17:43:22 +0300

    refactor: создание store вынесено в отдельный файл

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 // eslint-disable-next-line simple-import-sort/imports
 import "@ya.praktikum/react-developer-burger-ui-components";
-import { configureStore } from "@reduxjs/toolkit";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-import { rootReducer } from "./services/reducers";
+import { store } from "services/store";
 
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
@@ -15,10 +14,6 @@ import reportWebVitals from "./reportWebVitals";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/index.scss";
 
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== "production",
-});
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement);
 
 root.render(
